1e2f3293bccae4f5b296ac03044c9cabfc72aadc
Titus Wormer <tituswormer@gmail.com>
2020-02-13 11:25:16 +0100

    Add test for words

--- test.js
+++ test.js
@@ -4,12 +4,40 @@ var rhymes = require('.')
 
 test('rhymes', function(t) {
   var monkey = rhymes('monkey')
+  var words = monkey.map(d => d.word)
 
   t.equal(typeof rhymes, 'function', 'should be a function')
 
   t.ok(Array.isArray(monkey), 'monkey: should return an array')
   t.notEqual(monkey.length, 0, 'monkey: should not return an empty array')
   t.equal(typeof monkey[0], 'object', 'monkey: should return objects')
+
+  t.deepEqual(
+    words,
+    [
+      'chunky',
+      'clunky',
+      'funky',
+      'hunky',
+      'junkie',
+      'junky',
+      'punky',
+      'spunky',
+      'branki',
+      'cranky',
+      'denki',
+      'dinky',
+      'donkey',
+      'donkey(1)',
+      'francie',
+      'franke(1)',
+      'frankie',
+      'hankey',
+      'hanky',
+      'helsinki'
+    ],
+    'monkey: should result in rhymes!'
+  )
 
   t.doesNotThrow(function() {
     monkey.forEach(function(d, i) {
